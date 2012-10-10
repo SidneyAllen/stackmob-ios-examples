e4fd4970a8d0068c9b4e20eabe971fd5b4888742
Matt Vaznaian <matt@stackmob.com>
2012-10-10 12:54:30 -0700

    add custom code request example

---
+++ custom-code-request/custom-code-request/ViewController.m
@@ -0,0 +1,57 @@
+//
+//  ViewController.m
+//  base-project
+//
+//  Created by Matt Vaznaian on 9/28/12.
+//  Copyright (c) 2012 StackMob. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "AppDelegate.h"
+#import "StackMob.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+@synthesize managedObjectContext = _managedObjectContext;
+
+- (AppDelegate *)appDelegate {
+    return (AppDelegate *)[[UIApplication sharedApplication] delegate];
+}
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.managedObjectContext = [self.appDelegate managedObjectContext];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (IBAction)sendHelloWorld:(id)sender {
+    
+    SMCustomCodeRequest *request = [[SMCustomCodeRequest alloc]
+                                    initGetRequestWithMethod:@"hello_world"];
+    
+    [[[SMClient defaultClient] dataStore] performCustomCodeRequest:request onSuccess:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
+        NSLog(@"Success: %@",JSON);
+    } onFailure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON){
+        NSLog(@"Failure: %@",error);
+    }];
+    
+}
+@end
